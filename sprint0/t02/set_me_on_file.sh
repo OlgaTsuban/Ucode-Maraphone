ee65b5e9c87e6b6db84f8c721df8874996424cbf
Olga Tsuban <tsubanolga@gmail.com>
2023-08-28 12:12:04 +0300

    Sprint0

---
+++ sprint0/t02/set_me_on_file.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+filen="fire"
+touch -m -t 199108240000.00 "$filen" && chmod -wx "$filen" && chmod 400 "$filen"
---
+++ sprint0/t14/alias.sh
@@ -0,0 +1,1 @@
+echo -e "alies ga=\"git add\"\nalies gcmsg=\"git commit -m\"\nalies gp=\"git push\"" > $1
